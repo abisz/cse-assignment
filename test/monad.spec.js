0c2d1effac8efdd81098f6c5eeb1825177c8616a
Simon Reinsperger <reinsperger.smn@gmail.com>
2017-03-22 16:57:54 +0100

    test: adds test for method chaining

--- test/monad.spec.js
+++ test/monad.spec.js
@@ -4,13 +4,13 @@ const monad = require('../src/monad');
 
 test('monad', (assert) => {
   assert.equal(typeof monad(), 'function', 'is a function');
-  assert.deepEqual(monad([1, 2, 3])(), [1, 2, 3], 'init value is passed');
+  assert.deepEqual(monad([1, 2, 3])(), [1, 2, 3], 'init value was passed');
 
   assert.end();
 });
 
 test('monad.input()', (assert) => {
-    assert.deepEqual(monad().input([1, 2, 3])(), [1, 2, 3], 'input data is passed');
+    assert.deepEqual(monad().input([1, 2, 3])(), [1, 2, 3], 'input data was passed');
     assert.deepEqual(monad().input([1, 2, 3])(), monad([1, 2, 3])(), 'init value has same effect as input()');
     assert.end();
 });
@@ -19,7 +19,7 @@ test('monad.map()', (assert) => {
   const orig = [1, 2, 3];
 
   assert.deepEqual(monad(orig).map(e => e * 2)(), [2, 4, 6], 'output as expected');
-  assert.deepEqual(orig, [1, 2, 3], 'input data is not mutated');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
 
   assert.end();
 });
@@ -28,7 +28,7 @@ test('monad.reduce()', (assert) => {
   const orig = [1, 2, 3];
 
   assert.deepEqual(monad(orig).reduce((cur, prev) => cur + prev, 0)(), 6, 'output as expected');
-  assert.deepEqual(orig, [1, 2, 3], 'input data is not mutated');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
 
   assert.end();
 });
@@ -37,7 +37,7 @@ test('monad.odd()', (assert) => {
   const orig = [1, 2, 3];
 
   assert.deepEqual(monad(orig).odd()(), [1, 3], 'output as expected');
-  assert.deepEqual(orig, [1, 2, 3], 'input data is not mutated');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
 
   assert.end();
 });
@@ -46,7 +46,7 @@ test('monad.sum()', (assert) => {
   const orig = [1, 2, 3];
 
   assert.deepEqual(monad(orig).sum()(), 6, 'output as expected');
-  assert.deepEqual(orig, [1, 2, 3], 'input data is not mutated');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
 
   assert.end();
 });
@@ -55,7 +55,23 @@ test('monad.mul()', (assert) => {
   const orig = [1, 2, 3];
 
   assert.deepEqual(monad(orig).mul(2)(), [2, 4, 6], 'output as expected');
-  assert.deepEqual(orig, [1, 2, 3], 'input data is not mutated');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
+
+  assert.end();
+});
+
+test('monad method chaining', (assert) => {
+  const orig = [1, 2, 3];
+
+  assert.deepEqual(
+    monad()
+      .input(orig)
+      .odd()
+      .mul(2)
+      .sum()
+      ()
+    , 8, 'output as expected');
+  assert.deepEqual(orig, [1, 2, 3], 'input data was not mutated');
 
   assert.end();
 });
